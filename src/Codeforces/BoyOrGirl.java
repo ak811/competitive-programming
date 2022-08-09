9344f310cec9d716b5480f4df003b0871f8b3fe5
ak811 <ali.khaleghi.r@gmail.com>
2022-08-09 12:31:41 +0000

    Add: 271A-BoyOrGirl Solution

---
+++ src/Codeforces/BoyOrGirl.java
@@ -0,0 +1,40 @@
+/*
+    SubmissionId	    :	38572180
+	ContestId	        :	236
+	Index	            :	A
+	ProblemName         :	Beautiful Year
+	ProgrammingLanguage	:	Java
+	Verdict	            :	OK
+	Author              :   ak811
+    https://codeforces.com/contest/236/problem/A
+ */
+
+package Codeforces;
+
+import java.util.Scanner;
+
+public class BoyOrGirl {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+
+        int[] count = new int['z' + 1];
+
+        for (int i = 'a'; i <= 'z'; i++)
+            count[i] = 0;
+
+        for (int i = 0; i < str.length(); i++)
+            for (int j = 'a'; j <= 'z'; j++)
+                if (str.charAt(i) == (char) j)
+                    count[j]++;
+
+        int num = 0;
+        for (int i = 'a'; i <= 'z'; i++)
+            if (count[i] != 0)
+                num++;
+
+        System.out.println(num % 2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!");
+
+    }
+}
