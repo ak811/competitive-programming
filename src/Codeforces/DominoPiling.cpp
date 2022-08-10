42024bda086503f4a230c03f5a591864c9955b53
ak811 <ali.khaleghi.r@gmail.com>
2022-08-10 12:46:04 +0000

    Add: 50A-DominoPiling Solution

---
+++ src/Codeforces/DominoPiling.cpp
@@ -0,0 +1,21 @@
+/*
+    SubmissionId	    :	35316870
+	ContestId	        :	50
+	Index	            :	A
+	ProblemName         :	Domino Piling
+	ProgrammingLanguage	:	MS C++
+	Verdict	            :	OK
+	Author              :   ak811
+    https://codeforces.com/contest/50/problem/A
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n, m;
+	cin >> n >> m;
+	cout << (n*m) / 2;
+
+	return 0;
+}
