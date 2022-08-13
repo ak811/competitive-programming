cf82bbb768dd29b17842f2e27eb042017fb7facd
ak811 <ali.khaleghi.r@gmail.com>
2022-08-13 15:36:34 +0430

    Add: ACM-1293-Eniya Solution

---
+++ src/ACMTimusOnlineJudge/ACM-1293-Eniya.cpp
@@ -0,0 +1,22 @@
+/*
+	SubmissionId	    :	7747855
+	ContestId           :	1293
+	ProblemName         :	Eniya
+	ProgrammingLanguage :	Visual C++ 2017
+	Verdict             :	OK
+	Author              :   ak811
+	https://acm.timus.ru/problem.aspx?space=1&num=1293
+*/
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+	int n, a, b;
+	cin >> n >> a >> b;
+	cout << 2 * n * a * b;
+
+	getch();
+	return 0;
+}
