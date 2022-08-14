dd276b1a01d909faf3e7aa4dd1942d872a065c5d
ak811 <ali.khaleghi.r@gmail.com>
2022-08-14 15:00:46 +0430

    Add: ACM1264_Workdays Solution

---
+++ src/ACMTimusOnlineJudge/ACM1264_Workdays.cpp
@@ -0,0 +1,21 @@
+/*
+	SubmissionId	    :	7749413
+	ContestId           :	1264
+	ProblemName         :	Workdays
+	ProgrammingLanguage :	Visual C++ 2017
+	Verdict             :	OK
+	Author              :   ak811
+	https://acm.timus.ru/problem.aspx?space=1&num=1264
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int m, n;
+	cin >> n >> m;
+	cout << n * (m + 1);
+
+	return 0;
+}
