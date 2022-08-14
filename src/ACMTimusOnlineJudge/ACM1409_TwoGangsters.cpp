7072027c91399d69a60e7db1bedb0519acf51edc
ak811 <ali.khaleghi.r@gmail.com>
2022-08-14 14:59:11 +0430

    Add: ACM1409_TwoGangsters Solution

---
+++ src/ACMTimusOnlineJudge/ACM1409_TwoGangsters.cpp
@@ -0,0 +1,21 @@
+/*
+	SubmissionId	    :	7748006
+	ContestId           :	1409
+	ProblemName         :	Two Gangsters
+	ProgrammingLanguage :	Visual C++ 2017
+	Verdict             :	OK
+	Author              :   ak811
+	https://acm.timus.ru/problem.aspx?space=1&num=1409
+*/
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << b - 1 << " " << a - 1;
+	getch();
+	return 0;
+}
